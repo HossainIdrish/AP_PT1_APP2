5097c533b962d84eb7caf8c207dae53b2bb104bd
HossainIdrish <139071336+HossainIdrish@users.noreply.github.com>
2024-02-07 12:22:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ require([
     "esri/layers/FeatureLayer",
     "esri/widgets/Legend",
     "esri/widgets/LayerList",
-    "esri/widgets/Search" // Import the Search widget
+    "esri/widgets/Search"
 ], function(MapView, WebMap, FeatureLayer, Legend, LayerList, Search) {
     const webmap = new WebMap({
         portalItem: {
@@ -12,14 +12,30 @@ require([
         }
     });
 
+    // Assuming this is the "USA State Polygon" layer
+    const usaStatePolygonLayer = new FeatureLayer({
+        portalItem: {
+            id: "placeholder-for-USA-State-Polygon-layer-ID" // Placeholder ID for the "USA State Polygon" layer
+        }
+    });
+
+    // "USA Major Cities" layer
+    const usaMajorCitiesLayer = new FeatureLayer({
+        portalItem: {
+            id: "5ef295577fbc4541990d180ba82c6547" // ID of the "USA Major Cities" layer
+        }
+    });
+
     const additionalLayer = new FeatureLayer({
         portalItem: {
-            id: "662747c89e864d448b6060a206e0fc68" // ID of the new layer you want to add
+            id: "662747c89e864d448b6060a206e0fc68" // ID of the additional layer (if this represents another relevant dataset)
         }
     });
 
     webmap.load().then(() => {
-        webmap.add(additionalLayer); // Add the new layer after the WebMap has loaded
+        webmap.add(usaStatePolygonLayer); // Add the "USA State Polygon" layer first
+        webmap.add(usaMajorCitiesLayer); // Then add the "USA Major Cities" layer
+        webmap.add(additionalLayer); // Finally, add any additional layers
     });
 
     const view = new MapView({
@@ -51,3 +67,9 @@ require([
     // Add the Search widget to the view's UI
     view.ui.add(search, "top-left");
 });
+
+
+
+
+
+
